83ecf07493a454fb023f8d97d59dbe12a6cb1d3b
Christopher Smith <csmith4work@gmail.com>
2019-06-05 22:49:22 -0500

    Setting up app and S3 client.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-console.log("hello");
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const AWS = require('aws-sdk');
+AWS.config.update({
+    accessKeyId: process.env.NUREN_S3_IAM,
+    secretAccessKey: process.env.NUREN_S3_IAM_SECRET,
+    region: 'us-east-2'
+});
+const S3 = new AWS.S3();
+
+const app = express();
+const BUCKET_NAME = 'nurenproductions.com';
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/health', async (req, res) => {
+    res.send("Health check: Main API active.");
+});
+
+app.post('/', async (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
+});
+
+app.post('/stream/:video', async (req, res) => {
+    let videoParams = {
+        Bucket: BUCKET_NAME,
+        Key: req.params.video,
+    };
+    S3.getObject(videoParams, (err, data) => {
+        if(err){
+            console.log(err);
+        }
+        console.log(data);
+    });
+    res.send("ok");
+});
+
+app.listen(process.env.PORT || 8678);
+console.log("Api running");
